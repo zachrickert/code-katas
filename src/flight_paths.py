dc04fcc18174bf7e8875e86effdbc6faa8b53d4d
zachrickert <zachrickert@gmail.com>
2016-09-11 21:18:31 -0700

    Updated tests.

--- src/flight_paths.py
+++ src/flight_paths.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+"""Return a flight path between two airports."""
 from future.standard_library import install_aliases
 install_aliases()
 
@@ -23,40 +23,92 @@ def parse_json(data):
     return json_data
 
 
-def find_path(graph, start, end, path=[]):
-    path = path + [start]
-    if start == end:
-        return path
-    if start not in graph:
-        return None
-    for node in graph[start]:
-        if node not in path:
-            newpath = find_path(graph, node, path)
-            if newpath:
-                return newpath
-    return None
+def calculate_distance(point1, point2):
+    """
+    Calculate the distance (in miles) between point1 and point2.
+    point1 and point2 must have the format [latitude, longitude].
+    The return value is a float.
 
- # def find_path(graph, start, end, path=[]):
-    # path = path + [start]
-    # if start == end:
-    #     return path
-    # if not graph.has_key(start):
-    #     return None
-    # for node in graph[start]:
-    #     if node not in path:
-    #         newpath = find_path(graph, node, end, path)
-    #         if newpath:
-    #             return newpath
-    # return None
+    Modified and converted to Python from: http://www.movable-type.co.uk/scripts/latlong.html
+    """
+    import math
+
+    def convert_to_radians(degrees):
+        return degrees * math.pi / 180
+
+    radius_earth = 6.371E3 # km
+    phi1 = convert_to_radians(point1[0])
+    phi2 = convert_to_radians(point2[0])
+
+    delta_phi = convert_to_radians(point1[0] - point2[0])
+    delta_lam = convert_to_radians(point1[1] - point2[1])
+
+    a = math.sin(0.5 * delta_phi)**2 + math.cos(phi1) * math.cos(phi2) * math.sin(0.5 * delta_lam)**2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    return radius_earth * c / 1.60934 # convert km to miles
 
 
+def airport_lookup(json_data, airport):
+    """look-up an airport and return the dictionary with that entry."""
+    try:
+        return next((item for item in json_data if item["airport"] == airport))
+    except StopIteration:
+        raise NameError("{} not found in list of airports.".format(airport))
 
-data = parse_json(read_file(DATA_URL))
-import pdb; pdb.set_trace()
+
+def check_endpoint_airports(json_data, start, end):
+    """Return a NameError if either airport is not in data."""
+    airport_lookup(json_data, start)
+    airport_lookup(json_data, end)
+    return True
 
 
+def find_path(json_data, start_airport, end_airport):
+    """Does a breadth transversal until it finds the end airport."""
+    airport_list = [(start_airport, None)]
+    current_airport = start_airport
+    index = 0
+    is_done = False
+    while not is_done:
+        current_airport_dict = airport_lookup(json_data, current_airport)
+        for destination in current_airport_dict['destination_airports']:
+            airport_list.append((destination, index))
+            if destination == end_airport:
+                is_done = True
+                break
+        index += 1
+        current_airport = airport_list[index][0]
+    return airport_list
 
-# read_file
-# parse_json
-# find_path
-# return_path_and_distance
+
+def return_path(airport_list, json_data):
+    """Take list from find_path and return a list for a flight path."""
+    current_index = -1
+    start_airport = airport_list[current_index][0]
+    my_list = [start_airport]
+    start_airport_dict = airport_lookup(json_data, start_airport)
+    start_lat_lon = start_airport_dict['lat_lon']
+    milage = 0
+    while current_index:
+        current_index = airport_list[current_index][1]
+        end_airport = airport_list[current_index][0]
+        end_airport_dict = airport_lookup(json_data, end_airport)
+        end_lat_lon = end_airport_dict['lat_lon']
+        milage += calculate_distance(start_lat_lon, end_lat_lon)
+        my_list.append((airport_list[current_index][0]))
+        start_lat_lon = end_lat_lon
+
+    return my_list, milage
+
+
+def main(start, end):
+    """Take a start and end airports and return a path and milage."""
+    data = parse_json(read_file(DATA_URL))
+    check_endpoint_airports(data, start, end) 
+    search_path = find_path(data, start, end)
+    path = return_path(search_path, data)
+    print(path)
+
+
+if __name__ == '__main__':
+    main()
--- src/test_flight_paths.py
+++ src/test_flight_paths.py
@@ -3,8 +3,7 @@ import pytest
 
 from flight_paths import DATA_URL
 
-TEST_JSON = '''[{"city": "Goma", "connection_urls": ["https://en.wikipedia.org/wiki/Kindu_Airport", "https://en.wikipedia.org/wiki/Bangoka_International_Airport", "https://en.wikipedia.org/wiki/N%27djili_Airport", "https://en.wikipedia.org/wiki/Addis_Ababa_Bole_International_Airport"], "url": "https://en.wikipedia.org/wiki/Goma_International_Airport", "country": "Democratic Republic of the Congo", "destination_cities": ["Kinshasa", "Kisangani", "Addis Ababa"], "destination_airports": ["N'djili Airport", "Bangoka International Airport", "Addis Ababa Bole International Airport"], "airport": "Goma International Airport", "lat_lon": [-1.669889, 29.238278]}, {"city": "Kinshasa", "connection_urls": ["https://en.wikipedia.org/wiki/Port_Bouet_Airport", "https://en.wikipedia.org/wiki/Libreville_International_Airport", "https://en.wikipedia.org/wiki/Paris-Charles_de_Gaulle", "https://en.wikipedia.org/wiki/Gemena_Airport", "https://en.wikipedia.org/wiki/Kananga_Airport", "https://en.wikipedia.org/wiki/Mbandaka_Airport", "https://en.wikipedia.org/wiki/Mbuji_Mayi_Airport", "https://en.wikipedia.org/wiki/Douala_International_Airport", "https://en.wikipedia.org/wiki/Murtala_Muhammed_International_Airport", "https://en.wikipedia.org/wiki/Maya-Maya_Airport", "https://en.wikipedia.org/wiki/Libreville_International_Airport", "https://en.wikipedia.org/wiki/Lom%C3%A9%E2%80%93Tokoin_Airport", "https://en.wikipedia.org/wiki/Brussels_Airport", "https://en.wikipedia.org/wiki/Maya-Maya_Airport", "https://en.wikipedia.org/wiki/Douala_International_Airport", "https://en.wikipedia.org/wiki/Goma_International_Airport", "https://en.wikipedia.org/wiki/Kananga_Airport", "https://en.wikipedia.org/wiki/Kindu_Airport", "https://en.wikipedia.org/wiki/Bangoka_International_Airport", "https://en.wikipedia.org/wiki/Lubumbashi_International_Airport", "https://en.wikipedia.org/wiki/Mbandaka_Airport", "https://en.wikipedia.org/wiki/Mbuji_Mayi_Airport", "https://en.wikipedia.org/wiki/Bole_International_Airport", "https://en.wikipedia.org/wiki/Jomo_Kenyatta_International_Airport", "https://en.wikipedia.org/wiki/Mohammed_V_International_Airport", "https://en.wikipedia.org/wiki/OR_Tambo_International_Airport", "https://en.wikipedia.org/wiki/Quatro_de_Fevereiro_Airport", "https://en.wikipedia.org/wiki/Istanbul-Atat%C3%BCrk", "https://en.wikipedia.org/wiki/Libreville_International_Airport"], "url": "https://en.wikipedia.org/wiki/N%27djili_Airport", "country": "Democratic Republic of the Congo", "destination_cities": ["Goma", "Kisangani", "Lubumbashi", "Brazzaville", "Nairobi", "Casablanca", "Luanda", "Kempton Park", "Abidjan"], "destination_airports": ["Goma International Airport", "Bangoka International Airport", "Lubumbashi International Airport", "Maya-Maya Airport", "Jomo Kenyatta International Airport", "Mohammed V International Airport", "Quatro de Fevereiro Airport", "OR Tambo International Airport", "Port Bouet Airport"], "airport": "N'djili Airport", "lat_lon": [-4.38575, 15.4445694]}]'''
-
+TEST_JSON = '''[{"city": "Goma", "destination_airports": ["N'djili Airport", "Bangoka International Airport", "Addis Ababa Bole International Airport"], "airport": "Goma International Airport", "lat_lon": [-1.669889, 29.238278]}, {"city": "Kinshasa", "destination_airports": ["Goma International Airport", "Bangoka International Airport", "Lubumbashi International Airport", "Maya-Maya Airport", "Jomo Kenyatta International Airport", "Mohammed V International Airport", "Quatro de Fevereiro Airport", "OR Tambo International Airport", "Port Bouet Airport", "Test Airport"], "airport": "N'djili Airport", "lat_lon": [-4.38575, 15.4445694]},{"city": "Test", "destination_airports": ["Goma International Airport", "Bangoka International Airport", "Lubumbashi International Airport", "Maya-Maya Airport", "Jomo Kenyatta International Airport", "Mohammed V International Airport", "Quatro de Fevereiro Airport", "OR Tambo International Airport", "Port Bouet Airport"], "airport": "Test Airport", "lat_lon": [-4.38575, 15.4445694]}]'''
 
 def test_read_file():
     """Test to see if expected data is in file read."""
@@ -28,3 +27,44 @@ def test_parse_json():
     from flight_paths import parse_json
     json_data = parse_json(TEST_JSON)
     assert json_data[0]['airport'] == 'Goma International Airport'
+
+
+def test_airport_lookup():
+    """Test to see if airport lookup function is working correctly."""
+    from flight_paths import airport_lookup, parse_json
+    airport_dict = airport_lookup(parse_json(TEST_JSON), "N'djili Airport")
+    assert airport_dict['city'] == 'Kinshasa'
+
+
+def test_airport_lookup_not_in_data():
+    """Test to see if airport lookup function is working correctly."""
+    from flight_paths import airport_lookup, parse_json
+    with pytest.raises(NameError):
+        airport_dict = airport_lookup(parse_json(TEST_JSON), "Seattle-Tacoma International Airport")
+
+
+def test_check_endpoint_airports():
+    """Test to see if airport lookup function is working correctly."""
+    from flight_paths import check_endpoint_airports, parse_json
+    assert check_endpoint_airports(parse_json(TEST_JSON), "N'djili Airport", "Goma International Airport")
+
+
+def test_check_endpoint_airports_not_in_data():
+    """Test to see if airport lookup function is working correctly."""
+    from flight_paths import check_endpoint_airports, parse_json
+    with pytest.raises(NameError):
+        check_endpoint_airports(parse_json(TEST_JSON), "Seattle-Tacoma International Airport", "Goma International Airport")
+
+
+def test_find_path():
+    """Test to see if airport lookup function is working correctly."""
+    from flight_paths import find_path, parse_json
+    assert ("N'djili Airport", 0) in find_path(parse_json(TEST_JSON), "Goma International Airport", "Test Airport")
+
+
+def test_return_path():
+    """Test to see if airport lookup function is working correctly."""
+    from flight_paths import return_path, find_path, parse_json
+    flight_path = find_path(parse_json(TEST_JSON), "Goma International Airport", "Test Airport")
+    assert return_path(flight_path, parse_json(TEST_JSON))[0] == ["Test Airport", "N'djili Airport", "Goma International Airport"]
+
